6e83086e53425cb26fb788635d731fe8c8ab152d
shusuke <hokke0902@gmail.com>
2025-04-03 10:55:52 +0900

    移動処理の作成

---
+++ Assets/CarController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0;
+    Vector2 stratPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate=60;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            this.stratPos=Input.mousePosition;
+            Debug.Log(Input.mousePosition);
+        }else if(Input.GetMouseButtonUp(0)){
+            Vector2 endPos=Input.mousePosition;
+            float swipeLength = endPos.x - this.stratPos.x;
+            Debug.Log(Input.mousePosition);
+
+            this.speed=swipeLength/500.0f;
+        }        
+
+        transform.Translate(this.speed,0,0,Space.World);
+        this.speed *= 0.98f;
+    }
+}
